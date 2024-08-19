7d3a4e71d08985cd3816eb359a1be331a7d9f1c5
Frankie L <67761168+frankjlin16@users.noreply.github.com>
2024-08-18 21:48:39 -0700

    Initial commit

---
+++ src/test/java/org/codedevils/scraper/cd_scraper/CdScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codedevils.scraper.cd_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
